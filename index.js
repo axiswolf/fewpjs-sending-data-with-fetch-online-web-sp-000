959bfd14acf076ddb29cb2be8b164db102ac96b8
Karen V <karenvo@usc.edu>
2020-01-04 07:52:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ let configObj = {
   },
   body: JSON.stringify(formData)
 };
- 
-fetch("http://localhost:3000/users", configObj)
+
+  function submitData(fetch("http://localhost:3000/users", configObj)){
   .then(function(response) {
     return response.json();
   })
   .then(function(object) {
     console.log(object);
-  });
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  })};
