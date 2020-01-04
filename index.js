eb396f64f9433e5d2f796b83036c734e58885e3b
Karen V <karenvo@usc.edu>
2020-01-04 08:20:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 function submitData(name, email){
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+          name,
+          email
+        } )
+    };
 
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      appendIt(object.id);
+    })
+    .catch(function(error) {
+      appendIt(error.message);
+    });
+  }
 
-  let configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify( {
-        name,
-        email
-      } )
-  };
-
-  return fetch('http://localhost:3000/users', configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    appendIt(object.id);
-  })
-  .catch(function(error) {
-    appendIt(error.message);
-  });
-}
-
-function appendIt(it){
-  document.body.innerHTML = it;
+  function appendIt(it){
+    document.body.innerHTML = it;`
 }
