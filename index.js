bba2205db6a84d048e99b2a22e09bdc1cef4a0eb
Karen V <karenvo@usc.edu>
2020-01-04 08:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-// function submitData(name, email) {
-//   let formData = {
-//   name: name,
-//   email: email
-// };
-//
-// let configObj = {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json"
-//   },
-//   body: JSON.stringify(formData)
-// };
-//
-//   fetch("http://localhost:3000/users", configObj)
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(object) {
-//     console.log(object);
-//     document.getElementById("result").innerText = (object.id)
-//   })
-//   .catch(function(error) {
-//     alert("Unauthorized Access");
-//     console.log(error.message);
-//     document.getElementById("result").innerText = error.message
-//   });
-// }
+function submitData(name, email){
+}
 
-function submitData(name, email) {
-  return fetch("http://localhost:3000/users", {
-    method: 'POST',
-    body: {
-      name: name,
-      email: email
-    }
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify( {
+        name,
+        email
+      } )
+  };
+
+  return fetch('http://localhost:3000/users', configObj)
+  .then(function(response) {
+    return response.json();
   })
-  .then(function(result) => {
-      return result.json();
-  }).then(function(json){
-    return json.message;
+  .then(function(object) {
+    appendIt(object.id);
+  })
+  .catch(function(error) {
+    appendIt(error.message);
   });
+}
+
+function appendIt(it){
+  document.body.innerHTML = it;
 }
